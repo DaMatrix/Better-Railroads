94f2ac8689a7f024d1797c95afdfa5a6dcbd8011
MauveCloud <MauveCloud@users.noreply.github.com>
2019-04-18 13:22:12 -0700

    Added ability for mod's Empty Cart (formerly called "Faster Cart") to drop the mod item instead of a vanilla minecart
    See #12

--- src/main/java/dongle12/better_railroads/carts/MinecartEmpty.java
+++ src/main/java/dongle12/better_railroads/carts/MinecartEmpty.java
@@ -1,7 +1,10 @@
 package dongle12.better_railroads.carts;
 
+import dongle12.better_railroads.items.ModItems;
 import dongle12.better_railroads.util.ConfigHandler;
 import net.minecraft.entity.item.EntityMinecartEmpty;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 public class MinecartEmpty extends EntityMinecartEmpty{
@@ -41,4 +44,22 @@ public class MinecartEmpty extends EntityMinecartEmpty{
 			this.motionZ = connectedCart.motionZ;
 		}
 	}
+    
+    public void killMinecart(DamageSource source)
+    {
+        this.setDead();
+
+        if (this.world.getGameRules().getBoolean("doEntityDrops"))
+        {
+            ItemStack itemstack = new ItemStack(ModItems.empty_cart, 1);
+
+            if (this.hasCustomName())
+            {
+                itemstack.setStackDisplayName(this.getCustomNameTag());
+            }
+
+            this.entityDropItem(itemstack, 0.0F);
+        }
+    }
+    
 }
